31e7fcf0c70ede2de7dd3ff328012768f4819369
geremia <germysettesette@gmail.com>
2024-08-11 15:08:53 +0200

    tsserver added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 	config = function () 
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "javascript", "html" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },  
